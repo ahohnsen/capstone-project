db965e27963133692bf132c35600081e9a24b33d
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-15 10:58:47 +0100

    Update WishlistPage with storybook and tests

--- src/pages/WishlistPage.stories.js
+++ src/pages/WishlistPage.stories.js
@@ -14,10 +14,31 @@ WishlistPageDefault.args = {
       destination: 'Maldives',
       notes:
         'My last diving holidays in the Maldives were perfect! I wanna go there again as soon as possible. But next time I want to do a liveaboard.',
+      isArchived: false,
+      isBookmarked: true,
     },
     {
       destination: 'Lanzarote',
       notes: 'I want to go there again soon to see my former colleauges.',
+      isArchived: false,
+      isBookmarked: false,
+    },
+  ],
+};
+
+export const WishlistPageWithArchive = Template.bind({});
+WishlistPageWithArchive.args = {
+  diveWishes: [
+    {
+      destination: 'Maldives',
+      notes:
+        'My last diving holidays in the Maldives were perfect! I wanna go there again as soon as possible. But next time I want to do a liveaboard.',
+      isArchived: false,
+    },
+    {
+      destination: 'Lanzarote',
+      notes: 'I want to go there again soon to see my former colleauges.',
+      isArchived: true,
     },
   ],
 };
