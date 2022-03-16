b37a425f6b13961bf6d72cd7fd6457a8e582168e
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-16 17:50:21 +0100

    Create StartScreen page with storybook

---
+++ src/pages/StartScreen.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import Content from '../components/Content.js';
+import LoginForm from '../components/LoginForm.js';
+import BackgroundImage from '../images/Ocean.jpg';
+import ScubaMateLogo from '../images/ScubaMateLogo.svg';
+
+export default function StartScreen() {
+  const { login } = useParams();
+
+  return (
+    <Container>
+      <Logo src={ScubaMateLogo} alt="ScubaMate Logo" />
+      {login === 'login' && <LoginForm />}
+    </Container>
+  );
+}
+
+const Container = styled(Content)`
+  display: grid;
+  gap: 5px;
+  grid-template-rows: 1fr auto;
+  justify-items: center;
+  height: 100vh;
+  padding: 5px 5px 30px;
+  background: url(${BackgroundImage}) no-repeat center center;
+  background-size: cover;
+`;
+const Logo = styled.img`
+  width: 100%;
+`;
