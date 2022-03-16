3a23aeb37344cb8c9c9a7192f8950c67ee08a7f3
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-16 12:07:29 +0100

    Add Firebase config and authentification context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './firebase/AuthContext.js';
+import App from './App';
 import GlobalStyles from './GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyles />
-      <App />
+      <AuthProvider>
+        <GlobalStyles />
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
