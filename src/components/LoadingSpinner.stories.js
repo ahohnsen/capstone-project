af69c16837a243de4b73c7716fd7f7a40bbdb7c8
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-22 12:47:15 +0100

    Create component LoadingSpinner

---
+++ src/components/LoadingSpinner.stories.js
@@ -0,0 +1,8 @@
+import LoadingSpinner from './LoadingSpinner';
+
+export default {
+  title: 'Component/LoadingSpinner',
+  component: LoadingSpinner,
+};
+
+export const Default = <LoadingSpinner />;
