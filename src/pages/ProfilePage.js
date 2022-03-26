998d77db7418066e20810b87220bab8e2f5765bc
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-26 22:47:01 +0100

    Bugfix reloading of ProfilePage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -33,7 +33,7 @@ export default function ProfilePage({ sortedPosts }) {
       )
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [uid, currentUserData, users, sortedPosts]);
+  }, [uid, userProfileData, currentUserData]);
 
   return (
     <>
