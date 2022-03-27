a6c6c7713259956229c725f012da3c026edd98ee
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-27 20:37:13 +0200

    Update App and router

--- models/User.js
+++ models/User.js
@@ -6,6 +6,11 @@ const schema = new Schema(
     _id: { type: String, required: true },
     userId: { type: String, required: true },
     fullname: { type: String, required: true },
+    location: { type: String },
+    license: { type: String },
+    dives: { type: String },
+    facebook: { type: String },
+    about: { type: String },
   },
   {
     versionKey: false,
