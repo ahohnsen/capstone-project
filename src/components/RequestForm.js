388bb1ca341cbec45f0cc3e5dbc0ae2d98baae82
Andrea Hohnsen <a.hohnsen@mail.de>
2022-03-29 22:22:02 +0200

    Update component RequestForm

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import './MapboxGeocoderStyles.css';
 import Button from './Button.js';
+import IconButton from './IconButton.js';
+import DeleteIcon from '../images/DeleteIcon.svg';
 
 export default function RequestForm({
   formName,
@@ -9,15 +14,28 @@ export default function RequestForm({
   preloadedValues,
   handlePost,
 }) {
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESSTOKEN;
+
+  const [geocoderDestination, setGeocoderDestination] = useState(
+    preloadedValues?.destination ? preloadedValues?.destination : null
+  );
+  const [startDate, setStartDate] = useState(
+    preloadedValues?.startDate ? preloadedValues?.startDate : ''
+  );
+  const [endDate, setEndDate] = useState(
+    preloadedValues?.endDate ? preloadedValues?.endDate : ''
+  );
+  const dateToday = new Date().toISOString().substring(0, 10);
+
   const {
     register,
     handleSubmit,
     trigger,
-    formState: { errors },
+    formState: { isValid },
   } = useForm({
+    mode: 'all',
     defaultValues: preloadedValues
       ? {
-          destination: preloadedValues.destination,
           startDate: new Date(preloadedValues.startDate)
             .toISOString()
             .split('T')[0],
@@ -27,84 +45,114 @@ export default function RequestForm({
           description: preloadedValues.description,
         }
       : {
-          destination: '',
           startDate: '',
           endDate: '',
           description: '',
         },
   });
 
-  const dateToday = new Date().toISOString().substring(0, 10);
-  const [startDate, setStartDate] = useState('');
+  useEffect(() => {
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      types: 'country,region,place',
+      limit: 5,
+      clearOnBlur: true,
+    });
+
+    geocoder.addTo('#geocoderdestination');
+
+    geocoder.on('result', e => {
+      setGeocoderDestination(e.result.place_name);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <Form aria-label={formName} onSubmit={handleSubmit(post => onSubmit(post))}>
-      <Container>
-        <Label htmlFor="destination">DESTINATION</Label>
-        <Input
-          {...register('destination', {
-            required: 'A destination is required.',
-            maxLength: {
-              value: 25,
-              message: 'The name of the destination is too long.',
-            },
-          })}
-          id="destination"
-          onKeyUp={() => {
-            trigger('destination');
-          }}
-        />
-      </Container>
-      <div>
+    <>
+      <Form
+        aria-label={formName}
+        onSubmit={handleSubmit(post => onSubmit(post))}
+      >
+        <RequiredNote>Please note that all fields are required.</RequiredNote>
         <Container>
-          <Label htmlFor="start-date"> DATES - START AND END</Label>
-          <DateInput
-            id="start-date"
-            {...register('startDate', {
-              required: 'A start date is required',
+          <Label htmlFor="destination">DESTINATION</Label>
+          <DeleteButton
+            type="button"
+            onClick={() => setGeocoderDestination(null)}
+            hidden={!geocoderDestination}
+          >
+            <img src={DeleteIcon} alt="delete destination" />
+          </DeleteButton>
+          {geocoderDestination && (
+            <Input
+              id="destination"
+              readonly
+              disabled
+              value={geocoderDestination ? geocoderDestination : ''}
+            />
+          )}
+          <div id={'geocoderdestination'} hidden={geocoderDestination} />
+        </Container>
+        <div>
+          <Container>
+            <Label htmlFor="startDate"> DATES - START AND END</Label>
+            <DateInputStart
+              id="startDate"
+              {...register('startDate', {
+                required: true,
+              })}
+              type="date"
+              min={dateToday}
+              onChange={event => {
+                setStartDate(event.target.value);
+              }}
+            />
+            <Label htmlFor="endDate" className="sr-only">
+              END DATE
+            </Label>
+            <DateInputEnd
+              id="endDate"
+              {...register('endDate', { required: true })}
+              type="date"
+              min={startDate}
+              onChange={event => {
+                setEndDate(event.target.value);
+              }}
+            />
+          </Container>
+        </div>
+        <Container>
+          <Label htmlFor="description">DESCRIPTION</Label>
+          <Textarea
+            {...register('description', {
+              required: true,
+              maxLength: 500,
             })}
-            type="date"
-            min={dateToday}
-            onChange={event => {
-              setStartDate(event.target.value);
+            id="description"
+            onKeyUp={() => {
+              trigger('description');
             }}
-          />
-          <Label htmlFor="end-date" className="sr-only">
-            END DATE
-          </Label>
-          <DateInput
-            id="end-date"
-            {...register('endDate', { required: 'An end date is required' })}
-            type="date"
-            min={startDate}
+            maxLength="500"
           />
         </Container>
-      </div>
-      <Container>
-        <Label htmlFor="description">DESCRIPTION</Label>
-        <Textarea
-          {...register('description', {
-            required: ' A description is required.',
-          })}
-          id="description"
-          onKeyUp={() => {
-            trigger('description');
-          }}
-        />
-      </Container>
-      <Button type="submit">{buttonName}</Button>
-      <ErrorMessage>
-        <p>{errors.destination && errors.destination.message}</p>
-        <p>{errors.description && errors.description.message}</p>
-        <p>{errors.startDate && errors.startDate.message}</p>
-        <p>{errors.endDate && errors.endDate.message}</p>
-      </ErrorMessage>
-    </Form>
+        <SubmitButton
+          type="submit"
+          disabled={
+            geocoderDestination && isValid && startDate !== '' && endDate !== ''
+              ? false
+              : true
+          }
+        >
+          {buttonName}
+        </SubmitButton>
+      </Form>
+    </>
   );
 
   function onSubmit(post) {
     handlePost({
-      destination: post.destination,
+      destination: geocoderDestination,
       description: post.description,
       startDate: post.startDate,
       endDate: post.endDate,
@@ -118,6 +166,19 @@ const Form = styled.form`
   padding: 0 15px;
 `;
 
+const Container = styled.div`
+  position: relative;
+  padding: 12px 10px 8px;
+  border-radius: 4px;
+  background-color: var(--bg-color-section);
+  box-shadow: 1px 1px 4px var(--color-boxshadow);
+`;
+
+const RequiredNote = styled.span`
+  padding: 0 5px;
+  font-size: 0.9rem;
+`;
+
 const Label = styled.label`
   position: absolute;
   top: -13px;
@@ -127,23 +188,32 @@ const Label = styled.label`
 `;
 
 const Input = styled.input`
-  width: 100%;
-  padding: 2px;
+  padding: 2px 30px 2px 2px;
   border: 0;
+  color: var(--font-color-label);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
-const DateInput = styled.input`
-  padding: 2px;
-  border: 0;
-  width: 50%;
+const DeleteButton = styled(IconButton)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
 
-const Container = styled.div`
-  position: relative;
-  padding: 10px;
-  border-radius: 4px;
-  background-color: var(--bg-color-section);
-  box-shadow: 1px 1px 4px var(--color-boxshadow);
+const DateInputStart = styled.input`
+  padding: 2px;
+  border: 0;
+  width: 49%;
+
+  &:focus {
+    outline: thin dotted var(--bg-color-action);
+  }
+`;
+
+const DateInputEnd = styled(DateInputStart)`
+  margin-left: 2%;
 `;
 
 const Textarea = styled.textarea`
@@ -151,10 +221,12 @@ const Textarea = styled.textarea`
   height: 300px;
   padding: 2px;
   border: 0;
+
+  &:focus {
+    outline: thin dotted var(--bg-color-action);
+  }
 `;
 
-const ErrorMessage = styled.div`
-  text-align: center;
-  color: red;
-  font-size: 0.8rem;
+const SubmitButton = styled(Button)`
+  opacity: ${props => (props.disabled ? '0.5' : '1')};
 `;
